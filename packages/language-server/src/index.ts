84c09dff520c8f3dbfaf5a067ac25d2d7a833035
mizdra <pp.mizdra@gmail.com>
2023-10-21 23:37:43 +0900

    完成形

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,8 +1,7 @@
-import { language, Html1File } from './language';
-import createEmmetService from 'volar-service-emmet';
+import { language } from './language';
 import createHtmlService from 'volar-service-html';
 import createCssService from 'volar-service-css';
-import { createConnection, startLanguageServer, LanguageServerPlugin, Diagnostic, Service } from '@volar/language-server/node';
+import { createConnection, startLanguageServer, LanguageServerPlugin } from '@volar/language-server/node';
 
 const plugin: LanguageServerPlugin = (): ReturnType<LanguageServerPlugin> => ({
 	extraFileExtensions: [{ extension: 'html1', isMixedContent: true, scriptKind: 7 }],
@@ -16,31 +15,7 @@ const plugin: LanguageServerPlugin = (): ReturnType<LanguageServerPlugin> => ({
 		config.services ??= {};
 		config.services.html ??= createHtmlService();
 		config.services.css ??= createCssService();
-		config.services.emmet ??= createEmmetService();
-		config.services.html1 ??= (context): ReturnType<Service> => ({
-			provideDiagnostics(document) {
-
-				const [file] = context!.documents.getVirtualFileByUri(document.uri);
-				if (!(file instanceof Html1File)) return;
-
-				const styleNodes = file.htmlDocument.roots.filter(root => root.tag === 'style');
-				if (styleNodes.length <= 1) return;
-
-				const errors: Diagnostic[] = [];
-				for (let i = 1; i < styleNodes.length; i++) {
-					errors.push({
-						severity: 2,
-						range: {
-							start: file.document.positionAt(styleNodes[i].start),
-							end: file.document.positionAt(styleNodes[i].end),
-						},
-						source: 'html1',
-						message: 'Only one style tag is allowed.',
-					});
-				}
-				return errors;
-			},
-		});
+		config.services.html1 ??= (_serviceContext) => ({});
 
 		return config;
 	},
