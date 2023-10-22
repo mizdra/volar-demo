7a85cc8e24618136947feca9cdf1f5e08e2cb7e1
mizdra <pp.mizdra@gmail.com>
2023-10-22 15:54:39 +0900

    support language features

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,3 +1,6 @@
+import { language, createHtml1Service } from '@html1/core';
+import createHtmlService from 'volar-service-html';
+import createCssService from 'volar-service-css';
 import { createConnection, startLanguageServer, LanguageServerPlugin } from '@volar/language-server/node';
 
 const plugin: LanguageServerPlugin = (): ReturnType<LanguageServerPlugin> => ({
@@ -6,7 +9,18 @@ const plugin: LanguageServerPlugin = (): ReturnType<LanguageServerPlugin> => ({
 	resolveConfig(config) {
 		// ここに Volar.js の設定を書く
 
-		// ...
+		// 1. Language オブジェクトを登録。
+		// `.html1` の中身を解析して、どのような言語がどこに組み込まれているかを
+		// Volar.js に伝える役割を担っている。
+		config.languages ??= {};
+		config.languages.html1 ??= language;
+
+		// 2. `.html1` に組み込まれている言語のための Service を登録。
+		// Service とはエディタからのリクエストを受け取って、言語機能を提供するもののこと。
+		config.services ??= {};
+		config.services.html ??= createHtmlService();
+		config.services.css ??= createCssService();
+		config.services.html1 ??= createHtml1Service();
 
 		return config;
 	},
